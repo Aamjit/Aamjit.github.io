9b23984bbc61603cd1a9e4cbeb47c5bbf943b079
aamjit <aayanglem@gmail.com>
2020-12-14 12:30:38 +0530

    Added Profile Photo and Instagram link

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,16 +8,16 @@ function Home() {
         <Row>
             <Col>
                 <div className="d-flex justify-content-center align-items-center">
-                    <img src="https://cdn.shopify.com/s/files/1/1004/0238/files/IMG_0721_20e4394f-d3a8-438b-a670-f87004f59be6.jpg?10105982215108757311" className="m-5 shadow-lg" alt="photo" style={{borderRadius:"100%"}}/>
+                    <img src="https://avatars0.githubusercontent.com/u/54314099?s=460&u=f36910cf0b5cfac4691d8cbf4111c7c5dc4cd15a&v=4" className="m-5 shadow-lg" alt="photo" style={{borderRadius:"100%"}}/>
 
                 </div>
             </Col>
-            <Col className="d-flex justify-content-center align-items-center">
+            <Col className="d-flex justify-content-center align-items-center" style={{Row}}>
                 <h1 style={{ fontSize: "72px", color: ""}}>Aamjit</h1>
                 <h3 style={{color: "red"}}>React Newbie</h3>
 
                 <div>
-                <a className="m-2" href="" target="_blank">
+                <a className="m-2" href="https://www.instagram.com/aamjit_/"  rel="noreffer">
                     <FaInstagram color="#" fontSize="32px"/>
                 </a>
                 </div>
