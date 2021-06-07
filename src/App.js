3baf270430e639d3c8f04f2c237365ce81a84f41
Aamjit <54314099+Aamjit@users.noreply.github.com>
2021-06-07 21:51:41 +0530

    Revert "restructured the navigation bar"

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Footer from "./components/Footer";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div style={{ background: "#A5C9C8" }}>
       <Navigation />
@@ -14,9 +14,8 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
-      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
-import "./Components.scss";
 
 export default function Contact() {
   return (
-    <div className="container-fluid bg-" id="contact">
+    <div className="container-fluid bg-">
       <Row>
         <Col>
           <h1 style={{ color: "#05A9A6" }} className="text-center m-3">
@@ -14,37 +13,52 @@ export default function Contact() {
       </Row>
 
       <Row>
-        <Col xs={6} md={7} className="container-fluid text-darks">
-          <Form style={{ background: "" }}>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
+        <Col sm="10" lg="8" md="8" className="container-fluid text-light">
+          <Form style={{ background: "lightblue" }}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label
+                style={{ color: "#621232", width: "90%", padding: "14px" }}
+              >
+                Name
+              </Form.Label>
               <Form.Control type="text" name="name" placeholder="Enter Name" />
-              <Form.Text className="text-muted">
-                We'll never share your name with anyone else.
+              <Form.Text className="text-muted" style={{ paddingTop: "4px" }}>
+                We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Email address</Form.Label>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label
+                style={{ color: "#621232", width: "90%", padding: "14px" }}
+              >
+                Email address
+              </Form.Label>
               <Form.Control
                 type="email"
                 name="email"
-                placeholder="example@email.com"
+                placeholder="Enter email"
               />
-              <Form.Text className="text-muted">
-                We'll never share your e-mail with anyone else.
+              <Form.Text className="text-muted" style={{ paddingTop: "4px" }}>
+                We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Your Feedback</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label
+                style={{ color: "#621232", width: "90%", padding: "14px" }}
+              >
+                Enter your message
+              </Form.Label>
+              <Form.Control type="text" placeholder="Enter your message." />
             </Form.Group>
 
-            {/*<Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                style={{ color: "#621232" }}
+                type="checkbox"
+                label="Check me out"
+              />
             </Form.Group>
-  */}
             <Button variant="primary" type="submit">
               Submit
             </Button>
